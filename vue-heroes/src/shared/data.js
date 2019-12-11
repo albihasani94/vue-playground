44cd35435b188974594b85c1318f4d42a8f50365
albihasani94 <albihasani94@gmail.com>
2019-12-11 17:32:50 +0100

    Add environment variables

--- vue-heroes/src/shared/data.js
+++ vue-heroes/src/shared/data.js
@@ -1,9 +1,10 @@
 import * as axios from 'axios';
 import { format } from 'date-fns';
 import { inputDateFormat } from './constants';
+import { API } from './config';
 
 const getHeroes = async function () {
-  const response = await axios.get('api/heroes.json');
+  const response = await axios.get(`${API}/heroes.json`);
   const heroes = response.data.map(hero => {
     hero.originDate = format(new Date(hero.originDate), inputDateFormat);
     return hero;
--- vue-heroes/src/shared/index.js
+++ vue-heroes/src/shared/index.js
@@ -1,2 +1,4 @@
 export * from './my-mixins';
 export * from './data';
+export * from './config';
+export * from './constants';
