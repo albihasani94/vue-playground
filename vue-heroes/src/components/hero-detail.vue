852045afd75464df33cf8d53c2e1ad2fbf83aa0c
albihasani94 <albihasani94@gmail.com>
2019-12-11 12:11:05 +0100

    Refactor into heroes-hero-detail components

---
+++ vue-heroes/src/components/hero-detail.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-8">
+        <div class="card edit-detail">
+          <header class="card-header">
+            <!-- Using interpolation -->
+            <p class="card-header-title">{{ fullName }}</p>
+            <!-- same as v-text="fullName" -->
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <div class="field">
+                <label class="label" for="id">id</label>
+                <label class="input" id="id" readonly>{{ clonedHero.id }}</label>
+              </div>
+              <div class="field">
+                <label class="label" for="firstName">first name</label>
+                <input class="input" id="firstName" v-model="clonedHero.firstName" />
+              </div>
+              <div class="field">
+                <label for="show" class="checkbox">
+                  show more
+                  <input type="checkbox" class="is-primary" id="show" v-model="showMore" />
+                </label>
+              </div>
+              <div class="field" v-show="showMore">
+                <label class="label" for="lastName">last name</label>
+                <input class="input" id="lastName" v-model="clonedHero.lastName" />
+              </div>
+              <div class="field" v-show="showMore">
+                <label class="label" for="description">description</label>
+                <textarea class="input" id="description" type="text" v-model="clonedHero.description" />
+              </div>
+              <div class="field">
+                <label class="label">cape color</label>
+                <label class="radio" for="color-red">
+                  <input type="radio" id="color-red" value="red" v-model="clonedHero.capeColor" />
+                  red
+                </label>
+                <label class="radio" for="color-blue">
+                  <input type="radio" id="color-blue" value="blue" v-model="clonedHero.capeColor" />
+                  blue
+                </label>
+                <label class="radio" for="color-black">
+                  <input type="radio" id="color-black" value="black" v-model="clonedHero.capeColor" />
+                  black
+                </label>
+                <div class="color-line" :style="{ 'background-color': clonedHero.capeColor }"></div>
+              </div>
+              <div class="field">
+                <label for="power">
+                  super power
+                  <div class="select is-primary">
+                    <select
+                      id="power"
+                      v-model="clonedHero.power"
+                      @keyup.esc="clearPower"
+                      :class="{ invalid: !clonedHero.power }"
+                    >
+                      <option disabled value>Please select one</option>
+                      <option>Speed</option>
+                      <option>Flight</option>
+                      <option>Strength</option>
+                      <option>Being dark</option>
+                    </select>
+                  </div>
+                </label>
+              </div>
+              <div class="field">
+                <label class="checkbox" for="active">
+                  active
+                  <input type="checkbox" class="is-primary" id="active" v-model="clonedHero.active" />
+                </label>
+              </div>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <!-- full signature -->
+            <button class="link card-footer-item cancel-button" v-on:click="cancelHero">
+              <i class="fas fa-undo"></i>
+              <span>Cancel</span>
+            </button>
+            <!-- shortcut signature -->
+            <button class="link card-footer-item" @click="saveHero()">
+              <i class="fas fa-save"></i>
+              <span>Save</span>
+            </button>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroDetail',
+  props: {
+    hero: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      clonedHero: { ...this.hero },
+      showMore: false,
+    };
+  },
+  methods: {
+    cancelHero() {
+      // this.hero = undefined;
+      this.$emit('cancel');
+    },
+    saveHero() {
+      // this.message = JSON.stringify(this.clonedHero, null, '\n');
+      this.$emit('save', this.clonedHero);
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.clonedHero.firstName} ${this.clonedHero.lastName}`;
+    },
+  },
+};
+</script>
--- vue-heroes/src/components/heroes.vue
+++ vue-heroes/src/components/heroes.vue
@@ -3,8 +3,8 @@
     <div class="section content-title-group">
       <h2 class="title">Heroes</h2>
     </div>
-    <div class="columns">
-      <div class="column is-3" v-show="heroes.length">
+    <div class="columns" v-if="!selectedHero" >
+      <div class="column is-8" v-show="heroes.length">
         <header class="card-header">
           <p class="card-header-title">heroes list</p>
         </header>
@@ -24,115 +24,13 @@
         <pre>{{ message }}</pre>
       </div>
     </div>
-    <div class="columns" v-if="selectedHero">
-      <div class="column is-8">
-        <div class="card edit-detail">
-          <header class="card-header">
-            <!-- Using interpolation -->
-            <p class="card-header-title">{{ fullName }}</p>
-            <!-- same as v-text="fullName" -->
-          </header>
-          <div class="card-content">
-            <div class="content">
-              <div class="field">
-                <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly>{{ selectedHero.id }}</label>
-              </div>
-              <div class="field">
-                <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" v-model="selectedHero.firstName" />
-              </div>
-              <div class="field">
-                <label for="show" class="checkbox">
-                  show more
-                  <input type="checkbox" class="is-primary" id="show" v-model="showMore" />
-                </label>
-              </div>
-              <div class="field" v-show="showMore">
-                <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" v-model="selectedHero.lastName" />
-              </div>
-              <div class="field" v-show="showMore">
-                <label class="label" for="description">description</label>
-                <textarea
-                  class="input"
-                  id="description"
-                  type="text"
-                  v-model="selectedHero.description"
-                />
-              </div>
-              <div class="field">
-                <label class="label">cape color</label>
-                <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red" v-model="selectedHero.capeColor" />
-                  red
-                </label>
-                <label class="radio" for="color-blue">
-                  <input type="radio" id="color-blue" value="blue" v-model="selectedHero.capeColor" />
-                  blue
-                </label>
-                <label class="radio" for="color-black">
-                  <input
-                    type="radio"
-                    id="color-black"
-                    value="black"
-                    v-model="selectedHero.capeColor"
-                  />
-                  black
-                </label>
-                <div class="color-line" :style="{ 'background-color': selectedHero.capeColor }"></div>
-              </div>
-              <div class="field">
-                <label for="power">
-                  super power
-                  <div class="select is-primary">
-                    <select
-                      id="power"
-                      v-model="selectedHero.power"
-                      @keyup.esc="clearPower"
-                      :class="{ invalid: !selectedHero.power }"
-                    >
-                      <option disabled value>Please select one</option>
-                      <option>Speed</option>
-                      <option>Flight</option>
-                      <option>Strength</option>
-                      <option>Being dark</option>
-                    </select>
-                  </div>
-                </label>
-              </div>
-              <div class="field">
-                <label class="checkbox" for="active">
-                  active
-                  <input
-                    type="checkbox"
-                    class="is-primary"
-                    id="active"
-                    v-model="selectedHero.active"
-                  />
-                </label>
-              </div>
-            </div>
-          </div>
-          <footer class="card-footer">
-            <!-- full signature -->
-            <button class="link card-footer-item cancel-button" v-on:click="cancelHero">
-              <i class="fas fa-undo"></i>
-              <span>Cancel</span>
-            </button>
-            <!-- shortcut signature -->
-            <button class="link card-footer-item" @click="saveHero">
-              <i class="fas fa-save"></i>
-              <span>Save</span>
-            </button>
-          </footer>
-        </div>
-      </div>
-    </div>
+    <HeroDetail v-if="selectedHero" :hero="selectedHero"
+    @cancel="cancelHero" @save="saveHero" />
   </div>
 </template>
 
 <script>
+import HeroDetail from '@/components/hero-detail';
 const ourHeroes = [
   {
     id: 1,
@@ -173,25 +71,25 @@ export default {
   data() {
     return {
       selectedHero: undefined,
-      showMore: false,
       heroes: [],
       message: '',
     };
   },
-  computed: {
-    fullName() {
-      return `${this.selectedHero.firstName} ${this.selectedHero.lastName}`;
-    },
+  components: {
+    HeroDetail,
   },
   created() {
     this.loadHeroes();
   },
   methods: {
     cancelHero() {
-      this.message = '';
+      this.selectedHero = undefined;
     },
-    saveHero() {
-      this.message = JSON.stringify(this.selectedHero, null, '\n');
+    saveHero(hero) {
+      const index = this.heroes.findIndex(h => h.id === hero.id);
+      this.heroes.splice(index, 1, hero);
+      this.heroes = [...this.heroes];
+      this.selectedHero = undefined;
     },
     clearPower() {
       this.selectedHero.power = '';
